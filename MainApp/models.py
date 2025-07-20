d49b785f0ab832e02fb249f03316bff7772ee70b
mazemdevol <fidavam@gmail.com>
2025-07-20 10:47:20 +0200

    lesson 5 end

--- MainApp/models.py
+++ MainApp/models.py
@@ -1,7 +1,19 @@
 from django.db import models
 
+class Color(models.Model):
+    name = models.CharField(max_length=30)
+    hex_code = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+
 class Item(models.Model):
     name = models.CharField(max_length=100)
     brand = models.CharField(max_length=100)
     count = models.PositiveIntegerField()
     description = models.TextField(default="Описание товара отсутствует")
+    colors = models.ManyToManyField(to=Color)
+
+    def __str__(self):
+        return f"({self.name}) ({self.brand})"
